c805b543263c480b298305d27feffade7e12f464
Rodrigo <rodrigotapia90@gmail.com>
2024-04-29 19:02:37 -0400

    Fixed Cart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState } from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
@@ -10,17 +9,20 @@ import '../styles/App.css'
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [cart, setCart] = useState([]); // Inicializa cart como un array vacío
 
   return (
     <div className="app">
       <Header />
       <div className='main-content'>
         <div className="content">
-          <Sidebar />
+          <Sidebar cart={cart} /> {/* Pasa cart como prop a Sidebar */}
           <div className="content-right">
             {selectedCategory ? (
               <CategoryProducts
                 category={selectedCategory}
+                cart={cart} // Pasa cart como prop a CategoryProducts
+                setCart={setCart} // Pasa setCart como prop a CategoryProducts
               />
             ) : (
               <MenuOptions setSelectedCategory={setSelectedCategory} />
