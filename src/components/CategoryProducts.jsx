f9c2e178a60970ec378ab04a6104edba2c24f8d8
rodrigotapia90 <rodrigotapia90@gmail.com>
2024-04-29 17:09:29 +0000

    Fixed grids

--- src/components/CategoryProducts.jsx
+++ src/components/CategoryProducts.jsx
@@ -1,12 +1,19 @@
-// CategoryProducts.js
 import React from 'react';
+import '../styles/CategoryProducts.css';
 
-function CategoryProducts({ category, cart, setCart }) {
-  // Suponiendo que products es un arreglo de objetos con información sobre los productos de la categoría seleccionada
-  const products = [
+function CategoryProducts({ cart, setCart }) {
+  const sampleProducts = [
     { id: 1, name: 'Café', price: 2.5 },
     { id: 2, name: 'Torta de Chocolate', price: 3.0 },
-    // Otros productos...
+    { id: 3, name: 'Té verde', price: 2.0 },
+    { id: 4, name: 'Jugo de naranja', price: 2.0 },
+    { id: 5, name: 'Café con leche', price: 3.0 },
+    { id: 6, name: 'Galletas de avena', price: 1.5 },
+    { id: 7, name: 'Agua mineral', price: 1.0 },
+    { id: 8, name: 'Helado de vainilla', price: 3.5 },
+    { id: 9, name: 'Sándwich de pollo', price: 5.0 },
+    { id: 10, name: 'Pizza Margarita', price: 8.0 },
+    // Agrega más productos según sea necesario
   ];
 
   const addToCart = (product) => {
@@ -15,12 +22,12 @@ function CategoryProducts({ category, cart, setCart }) {
 
   return (
     <div className="category-products">
-      <h2>{category}</h2>
       <ul>
-        {products.map((product) => (
+        {sampleProducts.map((product) => (
           <li key={product.id}>
-            {product.name} - ${product.price}
-            <button onClick={() => addToCart(product)}>Agregar al carrito</button>
+            <button onClick={() => addToCart(product)}>
+              {product.name}
+            </button>
           </li>
         ))}
       </ul>
