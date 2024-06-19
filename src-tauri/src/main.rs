b7f2447f3375bf1a61045f3cdd5a3f859f9cb4af
Rodrigo <rodrigotapia90@gmail.com>
2024-06-19 15:40:05 -0400

    Added conection to the db, added 'create_database_schema', 'initialize_database', 'add_category', 'add_product' into main.rs. Changed also the icon to the png logo on assets.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,30 +1,138 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use rusqlite::{params, Connection};
+use std::fs;
+use rusqlite::{params, Connection, named_params};
+use tauri::{AppHandle, State, Manager};
+
+pub struct AppState {
+  pub db: std::sync::Mutex<Option<Connection>>,
+}
+
+fn initialize_database(app_handle: &AppHandle) -> Result<Connection, rusqlite::Error> {
+    let app_dir = app_handle.path_resolver().app_data_dir().expect("The app data directory should exist.");
+    fs::create_dir_all(&app_dir).expect("The app data directory should be created.");
+    let sqlite_path = app_dir.join("MyApp.sqlite");
+
+    let db = Connection::open(sqlite_path)?;
+
+    Ok(db)
+}
+
+pub fn create_database_schema(conn: &Connection) -> Result<(), rusqlite::Error> {
+    conn.execute_batch(
+        "
+        CREATE TABLE IF NOT EXISTS Categoria (
+            id_categoria INTEGER PRIMARY KEY,
+            nombre_categoria TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS Productos (
+            id_producto INTEGER PRIMARY KEY,
+            nombre_producto TEXT,
+            id_categoria INTEGER,
+            precio_producto INTEGER,
+            FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria)
+        );
+
+        CREATE TABLE IF NOT EXISTS Agregados (
+            id_agregado INTEGER PRIMARY KEY,
+            nombre_agregado TEXT,
+            costo_agregado INTEGER
+        );
+
+        CREATE TABLE IF NOT EXISTS Producto_Agregado (
+            id_producto INTEGER,
+            id_agregado INTEGER,
+            PRIMARY KEY (id_producto, id_agregado),
+            FOREIGN KEY (id_producto) REFERENCES Productos(id_producto),
+            FOREIGN KEY (id_agregado) REFERENCES Agregados(id_agregado)
+        );
+
+        CREATE TABLE IF NOT EXISTS Clientes_Fiados (
+            id_cliente INTEGER PRIMARY KEY,
+            nombre_cliente TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS Transacciones_Fiado (
+            id_transaccion_fiado INTEGER PRIMARY KEY,
+            id_cliente INTEGER,
+            id_producto INTEGER,
+            cantidad INTEGER,
+            fecha_venta INTEGER,
+            monto_pendiente INTEGER,
+            FOREIGN KEY (id_cliente) REFERENCES Clientes_Fiados(id_cliente),
+            FOREIGN KEY (id_producto) REFERENCES Productos(id_producto)
+        );
+
+        CREATE TABLE IF NOT EXISTS Boletas (
+            id_boleta INTEGER PRIMARY KEY,
+            fecha INTEGER,
+            metodo_pago TEXT,
+            total INTEGER
+        );
+
+        CREATE TABLE IF NOT EXISTS Detalle_Boleta (
+            id_detalle_boleta INTEGER PRIMARY KEY,
+            id_boleta INTEGER,
+            id_producto INTEGER,
+            cantidad INTEGER,
+            precio_unitario REAL, -- Cambiado a REAL para decimales
+            id_agregado INTEGER,
+            FOREIGN KEY (id_boleta) REFERENCES Boletas(id_boleta),
+            FOREIGN KEY (id_producto) REFERENCES Productos(id_producto),
+            FOREIGN KEY (id_agregado) REFERENCES Agregados(id_agregado)
+        );
+        ",
+    )?;
+
+    Ok(())
+}
+
+#[tauri::command]
+fn add_category(app_handle: AppHandle, nombre: &str) -> Result<(), String> {
+    let state = app_handle.state::<AppState>();
+    let mut conn = state.db.lock().unwrap();
+
+    if let Some(conn) = &mut *conn {
+        conn.execute(
+            "INSERT INTO Categoria (nombre_categoria) VALUES (?)",
+            params![nombre],
+        )
+        .map_err(|e| e.to_string())?;
+        Ok(())
+    } else {
+        Err("No se pudo obtener la conexión a la base de datos".to_string())
+    }
+}
+
+#[tauri::command]
+fn add_product(app_handle: AppHandle, nombre: &str, id_categoria: i32, precio: i32) -> Result<(), String> {
+    let state = app_handle.state::<AppState>();
+    let mut conn = state.db.lock().unwrap();
+
+    if let Some(conn) = &mut *conn {
+        conn.execute(
+            "INSERT INTO Productos (nombre_producto, id_categoria, precio_producto) VALUES (?, ?, ?)",
+            params![nombre, id_categoria, precio],
+        )
+        .map_err(|e| e.to_string())?;
+        Ok(())
+    } else {
+        Err("No se pudo obtener la conexión a la base de datos".to_string())
+    }
+}
+
 
 fn main() {
-    let conn = Connection::open("categories.db").expect("Cannot open database");
-
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS categories (
-                  id INTEGER PRIMARY KEY,
-                  name TEXT NOT NULL UNIQUE
-                  )",
-        params![],
-    ).expect("Error creating categories table");
-
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS products (
-                  id INTEGER PRIMARY KEY,
-                  name TEXT NOT NULL,
-                  price REAL NOT NULL,
-                  category_id INTEGER,
-                  FOREIGN KEY(category_id) REFERENCES categories(id)
-                  )",
-        params![],
-    ).expect("Error creating products table");
-
     tauri::Builder::default()
+        .manage(AppState { db: Default::default() })
+        .invoke_handler(tauri::generate_handler![add_category, add_product])
+        .setup(|app| {
+            let handle = app.handle();
+            let db = initialize_database(&handle).expect("Database initialize should succeed");
+            create_database_schema(&db).expect("Database schema creation should succeed");
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
